d25edda8d2cb9f57530ad14022a902435fe575df
htetzawphyo <htetzawphyo.hpn@gmail.com>
2022-12-06 15:05:53 +0630

    project setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import './assets/global.css'
 
 createApp(App).use(router).mount('#app')
+
+
---
+++ src/views/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1 >Welcome</h1>
+    <button class="btn btn-secondary">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
